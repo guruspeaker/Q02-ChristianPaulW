290afdc89d1571b244b41252715d491d48d6cab4
guruspeaker <32222625+guruspeaker@users.noreply.github.com>
2017-10-25 10:16:24 -0400

    Add project files.

---
+++ Q02-ChristianPaulW/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Q02_ChristianPaulW
+{
+	[Activity(Label = "Q02_ChristianPaulW", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+		}
+	}
+}
+
